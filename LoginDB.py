4d747446629fc94b2916720ba7a39ff8fe56ba50
MateusCorradini <64249204+MateusCorradini@users.noreply.github.com>
2020-05-13 20:12:48 -0300

    Create LoginDB.py

---
+++ LoginDB.py
@@ -0,0 +1,22 @@
+# Classe do DB dos usuarios (logins, senhas, nomes dos médicos e CRM's) <sujeito a alterações>
+
+import sqlite3
+
+
+class LoginDB:
+
+    def __init__(self):
+        self.conexao = sqlite3.connect('login.db')
+        self.createTable()
+
+    def createTable(self):
+        c = self.conexao.cursor()
+
+        c.execute("""create table if not exists login (
+                    idusuario integer primary key autoincrement,
+                    usuario text,
+                    senha text
+                    nome text,
+                    crm text)""")
+        self.conexao.commit()
+        c.close()
